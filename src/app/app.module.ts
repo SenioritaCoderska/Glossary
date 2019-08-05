3b8e765ff661d5dec64385b7617dae0e05cf7920
Izabela Winkler <iwinkler@ent.clariant.com>
2019-08-05 15:41:15 +0200

    authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { GlossarySearchComponent } from './components/glossary-search/glossary-search.component';
 import { GlossaryApprovalComponent } from './components/glossary-approval/glossary-approval.component';
 import { GlossaryPermissionComponent } from './components/glossary-permission/glossary-permission.component';
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
@@ -15,35 +14,8 @@ import { ToastrModule } from 'ngx-toastr';
 import { GlossaryWelcomeComponent } from './components/glossary-welcome/glossary-welcome.component';
 import { GlossaryAddComponent } from './components/glossary-add/glossary-add.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 
-
-// const routes: Routes = [
-
-//   {
-//     path: 'approval',
-//     component: GlossaryApprovalComponent,
-//     data: { title: 'Check Approvals'}
-//   },
-//   {
-//     path: 'add',
-//     component: GlossaryAddComponent
-//   },
-//   {
-//     path: 'search',
-//     component: GlossarySearchComponent
-//   },
-//   {
-//     path: 'home',
-//     component: GlossaryWelcomeComponent
-//   },
-//   { path: '',
-//     redirectTo: '/home',
-//     pathMatch: 'full'
-// },
-// { path: '**', component: PageNotFoundComponent }
-// ];
 
 @NgModule({
   declarations: [
